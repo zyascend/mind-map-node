3a273e91073c65ae322012c8232ddb2ab6d7fd7b
zhangyang_lab <zyascend@qq.com>
2021-07-23 17:31:36 +0800

    eslint配置

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ const router = require('koa-router')()
 
 router.prefix('/users')
 
-router.get('/', function (ctx, next) {
+router.get('/', (ctx, next) => {
   ctx.body = 'this is a users response!'
 })
 
-router.get('/bar', function (ctx, next) {
+router.get('/bar', (ctx, next) => {
   ctx.body = 'this is a users/bar response'
 })
 
