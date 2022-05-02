2d5fa18b281d3778a6c3733f63fd3cda42d01436
zy_nitro <zyascend@qq.com>
2022-05-02 20:16:37 +0800

    【新增】二维码登录相关接口

--- controller/code.js
+++ controller/code.js
@@ -1,14 +1,49 @@
 /**
  * 二维码登录相关
  */
+const jsonwebtoken = require('jsonwebtoken')
+const { tokenSecret } = require('../configs')
 const { customAlphabet } = require('nanoid')
-const { successResponse } = require('./utils')
+const { successResponse, errorResponse } = require('./utils')
+const redis = require('./redis')
 
 const nanoid = customAlphabet('1234567890abcdef', 7)
 
 class Docs {
   async generate(ctx) {
-    ctx.body = successResponse(nanoid())
+    const qid = nanoid()
+    await redis.set(qid, JSON.stringify({ status: 'UNUSED', data: {} }))
+    ctx.body = successResponse(qid)
+  }
+
+  async setStatus(ctx) {
+    const { status, data, qid } = ctx.request.body
+    if (status && qid) {
+      let userData = data
+      if (status === 'CONFIRMED') {
+        const { _id } = data
+        userData = {
+          user: data,
+          token: jsonwebtoken.sign({ id: _id }, tokenSecret, { expiresIn: '1h' })
+        }
+      }
+      await redis.set(qid, JSON.stringify({ status, data: userData }))
+      ctx.body = successResponse({ status, qid })
+    } else {
+      ctx.body = errorResponse('status or qid cannot be null')
+    }
+  }
+
+  async getStatus(ctx) {
+    const { qid } = ctx.request.query
+    const [result, err] = await redis.get(qid)
+    console.log(result, err)
+    if (!result || err) {
+      ctx.body = successResponse({ status: 'EXPIRED', data: {}, qid })
+    } else {
+      const { status, data } = JSON.parse(result)
+      ctx.body = successResponse({ status, data, qid })
+    }
   }
 }
 module.exports = new Docs()
--- routes/code.js
+++ routes/code.js
@@ -4,6 +4,8 @@ const code = require('../controller/code')
 router.prefix('/code')
 
 router.get('/generate', code.generate)
+router.get('/getStatus', code.getStatus)
+router.post('/setStatus', code.setStatus)
 // router.post('/setFolder/:id', auth, user.checkOwner, docs.setFolder)
 // router.post('/setDoc/:id', auth, user.checkOwner, docs.setDoc)
 // router.post('/setDocContent/:id/', auth, user.checkOwner, docs.setDocContent)
